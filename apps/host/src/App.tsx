05f518f1a9e3227672caea12867489272a07f795
Julia Llama <julia.persidskaia@gmail.com>
2026-03-27 14:12:30 +0200

    chore(host): update websocket to open only onClick

--- apps/host/src/App.tsx
+++ apps/host/src/App.tsx
@@ -1,15 +1,10 @@
 import { useState, useEffect } from 'react'
 import { usePingSocket } from './hook/usePingWebsocket'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
-
-// import type { ControllerMessage } from '@cactus-hampster/typeshare'
 
 function App() {
   const [count, setCount] = useState(0)
-  const { sendPing, lastMessage, isConnected } = usePingSocket()
+  const { sendPing, lastMessage, isConnected, status, onConnect } = usePingSocket()
 
   useEffect(() => {
     if (lastMessage) {
@@ -17,126 +12,38 @@ function App() {
     }
   }, [lastMessage]);
 
+  useEffect(() => {
+    if (!isConnected) {
+      setCount(0)
+    }
+  }, [isConnected])
+
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-        <div>
-          <p>
-            {`Connection: ${isConnected ? "true" : "false"}`}
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={sendPing}
-        >
-          Count is {count}
-        </button>
-        {lastMessage ? <p>{lastMessage}</p> : null}
-      </section>
+    <section id="center">
+      <div>
+        <h1>Get started</h1>
+      </div>
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+      <div>
+        <p>
+          {`Websocket status: ${isConnected ? "connected" : "disconnected"} (${status})`}
+        </p>
+      </div>
+      <button
+        className="counter"
+        onClick={() => {
+          if (!isConnected) {
+            onConnect()
+          } else {
+            setCount((count) => count + 1)
+            sendPing()
+          }
+        }}
+      >
+        {`Press to ${isConnected ? `Ping server ${count}` : 'connect'}`}
+      </button>
+      {lastMessage ? <p>{lastMessage}</p> : null}
+    </section>
   )
 }
 
--- apps/host/src/hook/usePingWebsocket.ts
+++ apps/host/src/hook/usePingWebsocket.ts
@@ -1,19 +1,28 @@
 import useWebSocket, { ReadyState } from 'react-use-websocket'
+import { useState } from 'react'
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const useSocket = (useWebSocket as any).default as typeof useWebSocket
 
-const SOCKET_URL = 'ws://localhost:8080/ping'; 
+const SOCKET_URL = 'ws://localhost:8080/ping';
 
 export const usePingSocket = () => {
+  const [connect, setConnect] = useState<boolean>(false);
   const { sendMessage, lastMessage, readyState, getWebSocket } = useSocket(SOCKET_URL, {
     onOpen: () => console.log("Connected to WebSocket: ", getWebSocket()?.url),
-	onClose: () => console.log("Disconnected", getWebSocket()?.url),
-	onMessage: (event) => console.log('Received:', event.data),
-	heartbeat: {
-		message: "ping",
-		interval: 30_000,
-	}
-  });
+    onClose: () => {
+      setConnect(false)
+      console.log("Disconnected", getWebSocket()?.url)
+    },
+    onMessage: (event) => console.log('Received:', event.data),
+    heartbeat: {
+      message: "ping",
+      interval: 30_000,
+    }
+  }, connect);
+
+  const onConnect = () => {
+    setConnect(true)
+  }
 
   const connectionStatus = {
     [ReadyState.CONNECTING]: 'Connecting',
@@ -28,5 +37,6 @@ export const usePingSocket = () => {
     lastMessage: lastMessage ? lastMessage.data : null,
     isConnected: readyState === ReadyState.OPEN,
     status: connectionStatus,
+    onConnect,
   };
 };
