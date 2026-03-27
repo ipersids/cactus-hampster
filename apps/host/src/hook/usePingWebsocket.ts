425de59589471a5dbea136ea7b6c435dc5507629
Julia Llama <julia.persidskaia@gmail.com>
2026-03-28 00:50:41 +0200

    chore(host): fix heartbeat and test shared types

--- apps/host/src/hook/usePingWebsocket.ts
+++ apps/host/src/hook/usePingWebsocket.ts
@@ -3,11 +3,22 @@ import { useState } from 'react'
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const useSocket = (useWebSocket as any).default as typeof useWebSocket
 
+import type { HostEvent } from '@cactus-hampster/typeshare'
+
 const SOCKET_URL = 'ws://localhost:8080/ping';
+
+function createHostEvent(event: HostEvent): HostEvent {
+  return event;
+}
 
 export const usePingSocket = () => {
   const [connect, setConnect] = useState<boolean>(false);
   const { sendMessage, lastMessage, readyState, getWebSocket } = useSocket(SOCKET_URL, {
+    // @TODO: handle
+    // - onError (physical connection failed (e.g., DNS error, Server down))
+    // - onClose (common and custom reasons to close connection)
+    // - onMessage (game backeng logic errors, connection stays alive)
+    // - silent heartbeat errors and reconnection
     onOpen: () => console.log("Connected to WebSocket: ", getWebSocket()?.url),
     onClose: () => {
       setConnect(false)
@@ -17,6 +28,7 @@ export const usePingSocket = () => {
     heartbeat: {
       message: "ping",
       interval: 30_000,
+      returnMessage: "pong"
     }
   }, connect);
 
@@ -32,8 +44,18 @@ export const usePingSocket = () => {
     [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
   }[readyState];
 
+  const ping: HostEvent = createHostEvent({
+    status: "success",
+    data: {
+      type: 'ping',
+      payload: {
+        message: "Hello from Host!"
+      }
+    }
+  })
+
   return {
-    sendPing: () => sendMessage('Ping'),
+    sendPing: () => sendMessage(JSON.stringify(ping)),
     lastMessage: lastMessage ? lastMessage.data : null,
     isConnected: readyState === ReadyState.OPEN,
     status: connectionStatus,
